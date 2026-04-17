b9c399beeaae0ddd71aaa127b3ece1bcf626694e
Christophe Fonteyne <christophe.fonteyne@hallebardiers.be>
2026-04-17 14:36:08 +0200

    ignore config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('GOOGLE_CLIENT_ID',        'YOUR_CLIENT_ID.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET',    'YOUR_CLIENT_SECRET');
-define('GOOGLE_REDIRECT_URI',     'https://SITE_URL/oauth_callback.php');
-define('GOOGLE_WORKSPACE_DOMAIN', 'yourdomain.be');
+define('GOOGLE_CLIENT_ID',        '385896885638-n04ea9a949ntehbc56aeimre6id5qcj4.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET',    'YOUR_CLIENGOCSPX-xSVrOqKpupInt0mDpPHjMYROTmiAT_SECRET');
+define('GOOGLE_REDIRECT_URI',     'https://bar.hallebardiers.be/oauth_callback.php');
+define('GOOGLE_WORKSPACE_DOMAIN', 'hallebardiers.be');
